f744323b8562541b62e80753b1f91c650d801973
Poulet Thomas <dark.flames0@gmail.com>
2011-11-20 20:43:33 +0100

    4deg

---
+++ 4-quatrieme/4degres/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+
+    cout << "Equation de la forme ax4+bx3+cx2+dx+e=0" << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cout << "Equation de la forme "<<a <<"x4+"<<b<<"x3+"<<c<<"x2+"<<d<<"x+"<<e<<"=0" << endl;
+    return 0;
+}
